1274a69728020dd95f6111843638373fae88bec7
Tamires <tchinaidmorita@upei.ca>
2026-02-19 10:39:43 -0400

    add_date() feito. add_ie() em progresso

--- python.py
+++ python.py
@@ -10,7 +10,7 @@ class emissao_guias:
         self.page = self.browser.new_page()
         self.sitram_url = "http://www2.sefaz.ce.gov.br/sitram-internet/masterDetailLancamento.do?method=prepareSearch&quiosque=n"
         url_file = Path(__file__).parent / "SITRAM.html" #path to file to don't open page all the time
-        self.page.goto(str(url_file)) # change it to sitram_url afterwards
+        self.page.goto(self.sitram_url) 
 
 # Add período fato gerador e inscrição estadual 
     
@@ -18,35 +18,58 @@ class emissao_guias:
     # #dataInicialFatoGerador
     # #dataFinalFatoGerador
     def add_date(self):
-        self.page.locator("#dataInicialFatoGerador").fill("01/01/2024") # o que vai mudar é o mês e o ano
-        self.page.locator("#dataFinalFatoGerador").fill("31/01/2024") # idem
+        self.page.locator("#dataInicialFatoGerador").clear(delay=100)
+        self.page.locator("#dataInicialFatoGerador").type("01/01/2024", delay=100) # quem vai colocar a data é o usuário
+        self.page.wait_for_timeout(1000)
+        self.page.locator("#dataFinalFatoGerador").type("31/01/2024", delay=100) # quem vai colocar a data é o usuário
+
+# Pesquisar
+    # #pesquisar
+    def search(self):
+        self.page.locator("#pesquisar").click()
     
-    # I.E.
+    # I.E. + pesquisar
     # #contribuinteCredenciado
     def add_ie(self):
         excel_path = Path(__file__).parent / "Ceara_filiais.xlsx"
         df = pd.read_excel(excel_path)
-        ie_number = df['I..E.']
-        self.page.locator("#contribuinteCredenciado").fill(ie_number[0]) # o que vai mudar é o número da I.E. de cada filial, então tem que ser um loop para preencher cada uma delas
-        time.sleep(3) # pode remover isso aqui
 
+        for ie_number in df["I..E."]:
+            
+            self.add_date()
+            self.page.wait_for_timeout(2000)
 
-# Pesquisar
-    # #pesquisar
-    
+            self.page.locator("#contribuinteCredenciado").fill(ie_number)
+            self.page.wait_for_timeout(1000)
+
+            self.search()
+            self.page.wait_for_timeout(1000)
+            
+            if self.page.locator("button:has-text('OK')").is_visible():
+                print(f"Error processing I.E. {ie_number}")
+                self.page.locator("button:has-text('OK')").click()
+                self.page.wait_for_timeout(1000)
+                continue
+            
+
+            self.save_pdf(ie_number, "01", "2024")
+            
 
 # Salvar o relatório em PDF - considerando que esse arquivo ta dentro de Apuração ICMS > Ceará
     # Nome do PDF vai ser o mesmo mês e ano que for colocado no período fato gerador
-    def save_pdf(self):
+    def save_pdf(self, ie_number, month, year):
         base_path = Path(__file__).parent
-        pdf_path = base_path / "save-here" / "file2.pdf"
+        pdf_name = f"ICMS ANTECIPADO E ST ({ie_number}) - REF {month}{year}.pdf"
+        pdf_path = base_path / "save-here" / pdf_name
         self.page.pdf(path=str(pdf_path))
 
 # Alterar o nome do relatório salvo para "ICMS ANTECIPADO E ST (n inscrição estadual) - REF mmYYYY.pdf"
     
-# 
+# Fechar o browser    
+    def close(self):
+        self.browser.close()
 
 if __name__ == "__main__":
     p = emissao_guias()
-    p.add_date()
     p.add_ie()
+    p.close()
