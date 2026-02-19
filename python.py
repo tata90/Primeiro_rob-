98452d0e2501053ec6bc6d69f6da80ef3514ba74
Tamires <tchinaidmorita@upei.ca>
2026-02-19 12:25:06 -0400

    save_excel() adicionado

--- python.py
+++ python.py
@@ -9,7 +9,6 @@ class emissao_guias:
         self.browser = self.p.chromium.launch(headless=False)
         self.page = self.browser.new_page()
         self.sitram_url = "http://www2.sefaz.ce.gov.br/sitram-internet/masterDetailLancamento.do?method=prepareSearch&quiosque=n"
-        url_file = Path(__file__).parent / "SITRAM.html" #path to file to don't open page all the time
         self.page.goto(self.sitram_url) 
 
 # Add período fato gerador e inscrição estadual 
@@ -19,9 +18,10 @@ class emissao_guias:
     # #dataFinalFatoGerador
     def add_date(self):
         self.page.locator("#dataInicialFatoGerador").clear()
-        self.page.locator("#dataInicialFatoGerador").type("01/01/2024", delay=100) # quem vai colocar a data é o usuário
+        self.page.locator("#dataInicialFatoGerador").type("01/01/2026", delay=100) # quem vai colocar a data é o usuário
         self.page.wait_for_timeout(1000)
-        self.page.locator("#dataFinalFatoGerador").type("31/01/2024", delay=100) # quem vai colocar a data é o usuário
+        self.page.locator("#dataFinalFatoGerador").clear()
+        self.page.locator("#dataFinalFatoGerador").type("31/01/2026", delay=100) # quem vai colocar a data é o usuário
 
 # Pesquisar
     # #pesquisar
@@ -50,21 +50,46 @@ class emissao_guias:
                 print(f"Error processing I.E. {ie_number}")
                 self.page.locator("button:has-text('OK')").click()
                 self.page.wait_for_timeout(1000)
+                continue
             except:
-                self.save_pdf(ie_number, "01", "2024") #de novo, mes e ano vem do usuário - resolve isso
-                pass
-            
+                self.save_pdf(ie_number, "01", "2024") # mes e ano vem do usuário - resolve isso
+                self.save_excel(ie_number, "01", "2024") # mes e ano vem do usuário - resolve isso
+                self.page.wait_for_timeout(1000)
 
 # Salvar o relatório em PDF - considerando que esse arquivo ta dentro de Apuração ICMS > Ceará
     # Nome do PDF vai ser o mesmo mês e ano que for colocado no período fato gerador
-    def save_pdf(self, ie_number, month, year):
+    def save_pdf(self, ie_number, month, year): # mes e ano vem do usuário - resolve isso
         base_path = Path(__file__).parent
-        pdf_name = f"ICMS ANTECIPADO E ST ({ie_number}) - REF {month}{year}.pdf"
-        pdf_path = base_path / "save-here" / pdf_name
+        pdf_name = f"ICMS ANTECIPADO E ST {ie_number} - REF {month}{year}.pdf"
+        pdf_path = base_path / f"{month}{year}" / "RELATORIOS" / pdf_name
         self.page.pdf(path=str(pdf_path))
+        self.page.wait_for_timeout(2000)
+
+# Salvar o excel
+    def save_excel(self, ie_number, month, year):
+
+        base_path = Path(__file__).parent
+        excel_name = f"ICMS ANTECIPADO E ST {ie_number} - REF {month}{year}.xlsx"
+        excel_path = base_path / f"{month}{year}" / "RELATORIOS" / excel_name
+
+        # #selecionarTodos
+        # "button em.export-excel"
+        self.page.locator("#selecionarTodos").check()
+        self.page.locator("button em.export-excel").click()
+
+        download_info = self.page.expect_download()
+        download = download_info.value
+        download.save_as(str(excel_path))
+        self.page.wait_for_timeout(2000)
+
+    
+# Volta pra página de pesquisa pra processar a próxima I.E. da lista
+    #def back(self):
+
 
 # Alterar o nome do relatório salvo para "ICMS ANTECIPADO E ST (n inscrição estadual) - REF mmYYYY.pdf"
-    
+
+
 # Fechar o browser    
     def close(self):
         self.browser.close()
@@ -72,5 +97,7 @@ class emissao_guias:
 if __name__ == "__main__":
     p = emissao_guias()
     p.add_ie()
-
     p.close()
+
+
+#data = str(input("Digite a data"))
